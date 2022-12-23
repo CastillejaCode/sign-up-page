53db996ad6832d5b85ab368a4ff7854ea36c5758
Julian Krzysiak <jkrzysiak13@gmail.com>
2022-12-22 18:53:29 -0800

    Add Password Confirmation Logic

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const submit = document.querySelector('.account-create');
+const pwd = document.querySelector('#pwd');
+const pwdConfirm = document.querySelector('#pwd-confirm');
+const pwdLabel = document.querySelector('.pwd-label');
+const pwdLabelConfirm = document.querySelector('.pwd-label-confirm');
+
+submit.addEventListener('click', (e) => {
+	e.preventDefault();
+	if (pwd.value !== pwdConfirm.value) {
+		pwd.classList.toggle('error');
+		pwdConfirm.classList.toggle('error');
+		pwdLabelConfirm.textContent =
+			pwdLabel.textContent = `❌ Passwords don't match! ❌`;
+		setTimeout((e) => {
+			pwd.classList.toggle('error');
+			pwdConfirm.classList.toggle('error');
+			pwdLabelConfirm.textContent = 'CONFIRM PASSWORD';
+			pwdLabel.textContent = 'PASSWORD';
+			pwdConfirm.value = pwd.value = '';
+		}, 3000);
+	}
+});
